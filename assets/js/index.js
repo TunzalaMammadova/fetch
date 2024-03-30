495b2c6981f167ffb3e8ab4e49c851ce8f43a966
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-03-31 03:55:05 +0400

    added file end version

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,48 @@
 
 
 
-async function getData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+// async function getData() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+//     const datas = await response.json();
+//     let str = "";
+//     let tBody = document.querySelector("tbody")
+//     datas.forEach(data => {
+//         str +=
+//             `<tr>
+//             <td>${data.id}</td>
+//             <td>${data.name}</td>
+//             <td>${data.email}</td>
+//         </tr>`
+
+
+//         tBody.innerHTML = str;
+
+//     });
+
+// }
+
+
+//getData();
+
+async function getFullData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const datas = await response.json();
     let str = "";
     let tBody = document.querySelector("tbody")
     datas.forEach(data => {
         str +=
             `<tr>
+            <td>${data.userId}</td>
             <td>${data.id}</td>
-            <td>${data.name}</td>
-            <td>${data.email}</td>
-        </tr>`
-
+            <td>${data.title}</td>
+            <td>${data.body}</td>
+           </tr>`
 
         tBody.innerHTML = str;
 
     });
 
+   
 }
 
-
-getData();
+getFullData();
